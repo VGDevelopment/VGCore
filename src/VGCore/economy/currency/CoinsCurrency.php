a6d3e0df9c4286251fd775c127f3d2e447493c44
CaptainDuck <jmdimayuga021@gmail.com>
2017-11-18 17:37:21 +0800

    Oops :P

--- src/VGCore/economy/currency/CoinsCurrency.php
+++ src/VGCore/economy/currency/CoinsCurrency.php
@@ -6,8 +6,8 @@ class CoinsCurreency{
     private $main;
     private $player;
     private $coins = 0;
-    public function __construct(SystemOS $main, string $player){
     
+    public function __construct(SystemOS $main, string $player){
         $this->main = $main;
         $this->player = $player;
         $path = $this->getPath();
@@ -25,9 +25,10 @@ class CoinsCurreency{
     public function getCoins() : int{
         return $this->coins;
     }
-    public function addMCoins($name, $amt){
+    public function addCoins($name, $amt){
         $this->coins += $amt;
     }
-    public function reduceMoney($name, $amt){
+    public function reduceCoins($name, $amt){
         $this->coins = $this->coins - $amt;
+    }
 }
