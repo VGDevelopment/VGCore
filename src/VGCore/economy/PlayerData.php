e33916e954a69c170c22ed8ecffe3bf2014921e7
CaptainDuck <jmdimayuga021@gmail.com>
2017-11-19 12:59:48 +0800

    Merge All Currencies To One Class to Prevent Complications.. Finish DB and Everything, need to work on Conversion.

---
+++ src/VGCore/economy/PlayerData.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace VGCore\economy;
+
+use pocketmine\Player;
+use VGCore\SystemOS;
+
+class PlayerData{
+
+	private $db;
+	private $plugin;
+
+	public function __construct(SystemOS $plugin){
+		$this->plugin = $plugin;
+
+		/////////////////////////// DATABASE DETAILS ///////////////////////////
+
+		$this->db = mysqli_connect(" ", " ", " ");
+
+		if($this->db->connect_error){
+			$this->plugin->getLogger()->critical("Could not connect to MySQL server: ".$this->db->connect_error);
+			return;
+		}
+		if(!$this->db->query("CREATE TABLE IF NOT EXISTS users(
+			username VARCHAR(20) PRIMARY KEY,
+			dollars FLOAT
+			gems FLOAT
+			coins FLOAT
+		);")){
+			$this->plugin->getLogger()->critical("Error creating table: " . $this->db->error);
+			return;
+		}
+	}
+
+	/////////////////////////// PLAYER ACCOUNT DETAILS ///////////////////////////
+
+	public function accountExists($player){
+		if($player instanceof Player){
+			$player = $player->getName();
+		}
+		$player = strtolower($player);
+
+		$result = $this->db->query("SELECT * FROM users WHERE username='".$this->db->real_escape_string($player)."'");
+		return $result->num_rows > 0 ? true:false;
+	}
+
+	public function createAccount($player){
+		if($player instanceof Player){
+			$player = $player->getName();
+		}
+		$player = strtolower($player);
+
+		if(!$this->accountExists($player)){
+			$this->db->query("INSERT INTO users (username, coins) VALUES ('".$this->db->real_escape_string($player)."', 5000);");
+			$this->db->query("INSERT INTO users (username, dollars) VALUES ('".$this->db->real_escape_string($player)."', 0);");
+			$this->db->query("INSERT INTO users (username, gems) VALUES ('".$this->db->real_escape_string($player)."', 10);");
+			return true;
+		}
+		return false;
+	}
+
+	public function removeAccount($player){
+		if($player instanceof Player){
+			$player = $player->getName();
+		}
+		$player = strtolower($player);
+
+		if($this->db->query("DELETE FROM users WHERE username='".$this->db->real_escape_string($player)."'") === true) return true;
+		return false;
+	}
+
+	/////////////////////////// DOLLAR CURRENCY ///////////////////////////
+
+	public function getDollars($player){
+		if($player instanceof Player){
+			$player = $player->getName();
+		}
+		$player = strtolower($player);
+		$dol = $this->db->query("SELECT dollars FROM users WHERE username='".$this->db->real_escape_string($player)."'");
+		$doll = $dol->fetch_array()[0] ?? false;
+		$dol->free();
+		return $doll;
+	}
+
+	public function setDollars($player, $amount){
+		if($player instanceof Player){
+			$player = $player->getName();
+		}
+		$player = strtolower($player);
+		$amount = (float) $amount;
+		return $this->db->query("UPDATE users SET dollars = $amount WHERE username='".$this->db->real_escape_string($player)."'");
+	}
+
+	public function addDollars($player, $amount){
+		if($player instanceof Player){
+			$player = $player->getName();
+		}
+		$player = strtolower($player);
+		$amount = (float) $amount;
+		return $this->db->query("UPDATE users SET dollars = dollars + $amount WHERE username='".$this->db->real_escape_string($player)."'");
+	}
+
+	public function reduceDollars($player, $amount){
+		if($player instanceof Player){
+			$player = $player->getName();
+		}
+		$player = strtolower($player);
+		$amount = (float) $amount;
+		return $this->db->query("UPDATE users SET dollars = dollars - $amount WHERE username='".$this->db->real_escape_string($player)."'");
+	}
+
+	/////////////////////////// GEMS CURRENCY ///////////////////////////
+
+	public function getGems($player){
+		if($player instanceof Player){
+			$player = $player->getName();
+		}
+		$player = strtolower($player);
+		$dol = $this->db->query("SELECT gems FROM users WHERE username='".$this->db->real_escape_string($player)."'");
+		$doll = $dol->fetch_array()[0] ?? false;
+		$dol->free();
+		return $doll;
+	}
+
+	public function setGems($player, $amount){
+		if($player instanceof Player){
+			$player = $player->getName();
+		}
+		$player = strtolower($player);
+		$amount = (float) $amount;
+		return $this->db->query("UPDATE users SET gems = $amount WHERE username='".$this->db->real_escape_string($player)."'");
+	}
+
+	public function addGems($player, $amount){
+		if($player instanceof Player){
+			$player = $player->getName();
+		}
+		$player = strtolower($player);
+		$amount = (float) $amount;
+		return $this->db->query("UPDATE users SET gems = gems + $amount WHERE username='".$this->db->real_escape_string($player)."'");
+	}
+
+	public function reduceGems($player, $amount){
+		if($player instanceof Player){
+			$player = $player->getName();
+		}
+		$player = strtolower($player);
+		$amount = (float) $amount;
+		return $this->db->query("UPDATE users SET gems = gems - $amount WHERE username='".$this->db->real_escape_string($player)."'");
+	}
+
+	/////////////////////////// COINS CURRENCY ///////////////////////////
+
+	public function getCoins($player){
+		if($player instanceof Player){
+			$player = $player->getName();
+		}
+		$player = strtolower($player);
+		$dol = $this->db->query("SELECT coins FROM users WHERE username='".$this->db->real_escape_string($player)."'");
+		$doll = $dol->fetch_array()[0] ?? false;
+		$dol->free();
+		return $doll;
+	}
+
+	public function setCoins($player, $amount){
+		if($player instanceof Player){
+			$player = $player->getName();
+		}
+		$player = strtolower($player);
+		$amount = (float) $amount;
+		return $this->db->query("UPDATE users SET coins = $amount WHERE username='".$this->db->real_escape_string($player)."'");
+	}
+
+	public function addCoins($player, $amount){
+		if($player instanceof Player){
+			$player = $player->getName();
+		}
+		$player = strtolower($player);
+		$amount = (float) $amount;
+		return $this->db->query("UPDATE users SET coins = coins + $amount WHERE username='".$this->db->real_escape_string($player)."'");
+	}
+
+	public function reduceCoins($player, $amount){
+		if($player instanceof Player){
+			$player = $player->getName();
+		}
+		$player = strtolower($player);
+		$amount = (float) $amount;
+		return $this->db->query("UPDATE users SET coins = coins - $amount WHERE username='".$this->db->real_escape_string($player)."'");
+	}
+
+	/////////////////////////// CURRENCY CONVERSION ///////////////////////////
+
+	/////////////////////////// DATABASE CLOSE ///////////////////////////
+
+	public function close(){
+	 	$this->db->close();
+	}
+}
