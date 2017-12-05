2fb65dcd6543785f2249d54526ab8267b55c4ec8
Lewis Brindley <jgenius0@gmail.com>
2017-12-05 21:00:21 +0000

    Instance of Enchantment and not int.

--- src/VGCore/enchantment/VanillaEnchantment.php
+++ src/VGCore/enchantment/VanillaEnchantment.php
@@ -2,11 +2,11 @@
 
 namespace VGCore\enchantment;
 
-use pocketmine\item\enchantment\Enchantment as EM;
+use pocketmine\item\enchantment\Enchantment;
 // >>>
 use VGCore\SystemOS;
 
-class VanillaEnchantment extends EM {
+class VanillaEnchantment extends Enchantment {
     
     public $plugin;
     
@@ -15,28 +15,28 @@ class VanillaEnchantment extends EM {
     }
     
     public function registerEnchant() {
-        $this->registerEnchantment(EM::BLAST_PROTECTION, "%enchantment.protect.explosion", EM::RARITY_UNCOMMON, EM::ACTIVATION_EQUIP, EM::SLOT_ARMOR);
-		$this->registerEnchantment(EM::PROJECTILE_PROTECTION, "%enchantment.protect.projectile", EM::RARITY_UNCOMMON, EM::ACTIVATION_EQUIP, EM::SLOT_ARMOR);
-		$this->registerEnchantment(EM::THORNS, "%enchantment.protect.thorns", EM::RARITY_UNCOMMON, EM::ACTIVATION_EQUIP, EM::SLOT_SWORD);
-		$this->registerEnchantment(EM::RESPIRATION, "%enchantment.protect.waterbrething", EM::RARITY_UNCOMMON, EM::ACTIVATION_EQUIP, EM::SLOT_FEET);
-		$this->registerEnchantment(EM::DEPTH_STRIDER, "%enchantment.waterspeed", EM::RARITY_UNCOMMON, EM::ACTIVATION_EQUIP, EM::SLOT_FEET);
-		$this->registerEnchantment(EM::AQUA_AFFINITY, "%enchantment.protect.wateraffinity", EM::RARITY_UNCOMMON, EM::ACTIVATION_EQUIP, EM::SLOT_FEET);
-		$this->registerEnchantment(EM::SHARPNESS, "%enchantment.weapon.sharpness", EM::RARITY_UNCOMMON, EM::ACTIVATION_EQUIP, EM::SLOT_SWORD);
-		$this->registerEnchantment(EM::SMITE, "%enchantment.weapon.smite", EM::RARITY_UNCOMMON, EM::ACTIVATION_EQUIP, EM::SLOT_SWORD);
-		$this->registerEnchantment(EM::BANE_OF_ARTHROPODS, "%enchantment.weapon.arthropods", EM::RARITY_UNCOMMON, EM::ACTIVATION_EQUIP, EM::SLOT_SWORD);
-		$this->registerEnchantment(EM::KNOCKBACK, "%enchantment.weapon.knockback", EM::RARITY_UNCOMMON, EM::ACTIVATION_EQUIP, EM::SLOT_SWORD);
-		$this->registerEnchantment(EM::FIRE_ASPECT, "%enchantment.weapon.fireaspect", EM::RARITY_UNCOMMON, EM::ACTIVATION_EQUIP, EM::SLOT_SWORD);
-		$this->registerEnchantment(EM::LOOTING, "%enchantment.weapon.looting", EM::RARITY_UNCOMMON, EM::ACTIVATION_EQUIP, EM::SLOT_SWORD);
-		$this->registerEnchantment(EM::EFFICIENCY, "%enchantment.mining.efficiency", EM::RARITY_UNCOMMON, EM::ACTIVATION_EQUIP, EM::SLOT_TOOL);
-		$this->registerEnchantment(EM::SILK_TOUCH, "%enchantment.mining.silktouch", EM::RARITY_UNCOMMON, EM::ACTIVATION_EQUIP, EM::SLOT_TOOL);
-		$this->registerEnchantment(EM::UNBREAKING, "%enchantment.mining.durability", EM::RARITY_UNCOMMON, EM::ACTIVATION_EQUIP, EM::SLOT_TOOL);
-		$this->registerEnchantment(EM::FORTUNE, "%enchantment.mining.fortune", EM::RARITY_UNCOMMON, EM::ACTIVATION_EQUIP, EM::SLOT_TOOL);
-		$this->registerEnchantment(EM::POWER, "%enchantment.bow.power", EM::RARITY_UNCOMMON, EM::ACTIVATION_EQUIP, EM::SLOT_BOW);
-		$this->registerEnchantment(EM::PUNCH, "%enchantment.bow.knockback", EM::RARITY_UNCOMMON, EM::ACTIVATION_EQUIP, EM::SLOT_BOW);
-		$this->registerEnchantment(EM::FLAME, "%enchantment.bow.flame", EM::RARITY_UNCOMMON, EM::ACTIVATION_EQUIP, EM::SLOT_BOW);
-		$this->registerEnchantment(EM::INFINITY, "%enchantment.bow.infinity", EM::RARITY_UNCOMMON, EM::ACTIVATION_EQUIP, EM::SLOT_BOW);
-		$this->registerEnchantment(EM::LUCK_OF_THE_SEA, "%enchantment.fishing.fortune", EM::RARITY_UNCOMMON, EM::ACTIVATION_EQUIP, EM::SLOT_FISHING_ROD);
-		$this->registerEnchantment(EM::LURE, "%enchantment.fishing.lure", EM::RARITY_UNCOMMON, EM::ACTIVATION_EQUIP, EM::SLOT_FISHING_ROD);
+        $this->registerEnchantment(new Enchantment(self::BLAST_PROTECTION, "%enchantment.protect.explosion", self::RARITY_UNCOMMON, self::ACTIVATION_EQUIP, self::SLOT_ARMOR));
+		$this->registerEnchantment(new Enchantment(self::PROJECTILE_PROTECTION, "%enchantment.protect.projectile", self::RARITY_UNCOMMON, self::ACTIVATION_EQUIP, self::SLOT_ARMOR));
+		$this->registerEnchantment(new Enchantment(self::THORNS, "%enchantment.protect.thorns", self::RARITY_UNCOMMON, self::ACTIVATION_EQUIP, self::SLOT_SWORD));
+		$this->registerEnchantment(new Enchantment(self::RESPIRATION, "%enchantment.protect.waterbrething", self::RARITY_UNCOMMON, self::ACTIVATION_EQUIP, self::SLOT_FEET));
+		$this->registerEnchantment(new Enchantment(self::DEPTH_STRIDER, "%enchantment.waterspeed", self::RARITY_UNCOMMON, self::ACTIVATION_EQUIP, self::SLOT_FEET));
+		$this->registerEnchantment(new Enchantment(self::AQUA_AFFINITY, "%enchantment.protect.wateraffinity", self::RARITY_UNCOMMON, self::ACTIVATION_EQUIP, self::SLOT_FEET));
+		$this->registerEnchantment(new Enchantment(self::SHARPNESS, "%enchantment.weapon.sharpness", self::RARITY_UNCOMMON, self::ACTIVATION_EQUIP, self::SLOT_SWORD));
+		$this->registerEnchantment(new Enchantment(self::SMITE, "%enchantment.weapon.smite", self::RARITY_UNCOMMON, self::ACTIVATION_EQUIP, self::SLOT_SWORD));
+		$this->registerEnchantment(new Enchantment(self::BANE_OF_ARTHROPODS, "%enchantment.weapon.arthropods", self::RARITY_UNCOMMON, self::ACTIVATION_EQUIP, self::SLOT_SWORD));
+		$this->registerEnchantment(new Enchantment(self::KNOCKBACK, "%enchantment.weapon.knockback", self::RARITY_UNCOMMON, self::ACTIVATION_EQUIP, self::SLOT_SWORD));
+		$this->registerEnchantment(new Enchantment(self::FIRE_ASPECT, "%enchantment.weapon.fireaspect", self::RARITY_UNCOMMON, self::ACTIVATION_EQUIP, self::SLOT_SWORD));
+		$this->registerEnchantment(new Enchantment(self::LOOTING, "%enchantment.weapon.looting", self::RARITY_UNCOMMON, self::ACTIVATION_EQUIP, self::SLOT_SWORD));
+		$this->registerEnchantment(new Enchantment(self::EFFICIENCY, "%enchantment.mining.efficiency", self::RARITY_UNCOMMON, self::ACTIVATION_EQUIP, self::SLOT_TOOL));
+		$this->registerEnchantment(new Enchantment(self::SILK_TOUCH, "%enchantment.mining.silktouch", self::RARITY_UNCOMMON, self::ACTIVATION_EQUIP, self::SLOT_TOOL));
+		$this->registerEnchantment(new Enchantment(self::UNBREAKING, "%enchantment.mining.durability", self::RARITY_UNCOMMON, self::ACTIVATION_EQUIP, self::SLOT_TOOL));
+		$this->registerEnchantment(new Enchantment(self::FORTUNE, "%enchantment.mining.fortune", self::RARITY_UNCOMMON, self::ACTIVATION_EQUIP, self::SLOT_TOOL));
+		$this->registerEnchantment(new Enchantment(self::POWER, "%enchantment.bow.power", self::RARITY_UNCOMMON, self::ACTIVATION_EQUIP, self::SLOT_BOW));
+		$this->registerEnchantment(new Enchantment(self::PUNCH, "%enchantment.bow.knockback", self::RARITY_UNCOMMON, self::ACTIVATION_EQUIP, self::SLOT_BOW));
+		$this->registerEnchantment(new Enchantment(self::FLAME, "%enchantment.bow.flame", self::RARITY_UNCOMMON, self::ACTIVATION_EQUIP, self::SLOT_BOW));
+		$this->registerEnchantment(new Enchantment(self::INFINITY, "%enchantment.bow.infinity", self::RARITY_UNCOMMON, self::ACTIVATION_EQUIP, self::SLOT_BOW));
+		$this->registerEnchantment(new Enchantment(self::LUCK_OF_THE_SEA, "%enchantment.fishing.fortune", self::RARITY_UNCOMMON, self::ACTIVATION_EQUIP, self::SLOT_FISHING_ROD));
+		$this->registerEnchantment(new Enchantment(self::LURE, "%enchantment.fishing.lure", self::RARITY_UNCOMMON, self::ACTIVATION_EQUIP, self::SLOT_FISHING_ROD));
     }
     
 }
