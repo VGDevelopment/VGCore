4e07aebb272bbe205856822b06006ce5bb05d4c5
Lewis Brindley <jgenius0@gmail.com>
2017-12-22 00:59:08 +0000

    Rewrite of crates.

--- src/VGCore/lobby/crate/Crate.php
+++ src/VGCore/lobby/crate/Crate.php
@@ -22,32 +22,30 @@ class Crate {
     
     private static $crate1 = [142, 7, 109];
     private static $air1 = [142, 8, 109];
+    
     private static $crate2 = [137, 7, 106];
+    private static $air2 = [137, 8, 106];
+    
     private static $crate3 = [132, 7, 109];
-    private static $crate1text = [142, 8, 109];
-    private static $crate2text = [138, 8, 107];
-    private static $crate3text = [133, 8, 109];
+    private static $air3 = [132, 8, 109];
+    
+    private static $crate1text = [142, 10, 109];
+    private static $crate2text = [137, 10, 107];
+    private static $crate3text = [133, 10, 109];
     
     public static function turnOn(SystemOS $plugin) {
         $server = new VGServer($plugin);
         $servercheck = $server->checkServer();
         if ($servercheck === "Lobby") {
             $crate = [self::$crate1, self::$crate2, self::$crate3];
-            $cratetext = [self::$crate1text, self::$crate2text, self::$crate3text];
             $crateblock = Block::get(146, 1);
             $level = $plugin->getServer()->getLevelByName("Sam2");
-            $pos = new Vector3($crate[0][0], $crate[0][1], $crate[0][2]);
-            $textpos = new Vector3($cratetext[0][0], $cratetext[0][1], $cratetext[0][2]);
-            self::setTitle($level, $textpos);
-            $level->setBlock($pos, $crateblock);
-            $pos = new Vector3($crate[1][0], $crate[1][1], $crate[1][2]);
-            $textpos = new Vector3($cratetext[1][0], $cratetext[1][1], $cratetext[1][2]);
-            self::setTitle($level, $textpos);
-            $level->setBlock($pos, $crateblock);
-            $pos = new Vector3($crate[2][0], $crate[2][1], $crate[2][2]);
-            $textpos = new Vector3($cratetext[2][0], $cratetext[2][1], $cratetext[2][2]);
-            self::setTitle($level, $textpos);
-            $level->setBlock($pos, $crateblock);
+            foreach ($crate as $c) {
+                $pos = new Vector3($c[0], $c[1], $c[2]);
+                $level->setBlock($pos, $crateblock);
+            }
+            self::setAir($level);
+            self::setTitle($level);
         }
     }
     
@@ -58,19 +56,29 @@ class Crate {
             $crate = [self::$crate1, self::$crate2, self::$crate3];
             $level = $plugin->getServer()->getLevelByName("Sam2");
             $air = Block::get(0);
-            $pos = new Vector3($crate[0][0], $crate[0][1], $crate[0][2]);
-            $level->setBlock($pos, $air);
-            $pos = new Vector3($crate[1][0], $crate[1][1], $crate[1][2]);
-            $level->setBlock($pos, $air);
-            $pos = new Vector3($crate[2][0], $crate[2][1], $crate[2][2]);
-            $level->setBlock($pos, $air);
+            foreach ($crate as $c) {
+                $pos = new Vector3($c[0], $c[1], $c[2]);
+                $level->setBlock($pos, $air);
+            }
         }
     }
     
-    public static function setTitle(Level $level, Vector3 $pos, $title = "§k-- §r§e§lCRATE §k--", $text = "§c-----") {
+    private static function setTitle(Level $level, $text = "§k-- §r§e§lCRATE §k--") {
         $cratetext = [self::$crate1text, self::$crate2text, self::$crate3text];
-        $floatingent = new FTP($pos, $text, $title);
-        $level->addParticle($floatingent);
+        foreach ($cratetext as $ct) {
+            $pos = new Vector3($ct[0], $ct[1], $ct[2]);
+            $floatingent = new FTP($pos, $text);
+            $level->addParticle($floatingent);
+        }
+    }
+    
+    private static function setAir(Level $level) {
+        $air = [self::$air1, self::$air2, self::$air3];
+        foreach ($air as $a) {
+            $airblock = Block::get(0);
+            $pos = new Vector3($a[0], $a[1], $a[2]);
+            $level->setBlock($pos, $airblock);
+        }
     }
     
 }
