630e54180eff572ec89a3e6ed437d6e323381add
YaBoiLewis <jgenius0@gmail.com>
2018-01-21 14:21:03 +0000

    Class imports

--- src/VGCore/form/BasicUI.php
+++ src/VGCore/form/BasicUI.php
@@ -2,6 +2,24 @@
 
 namespace VGCore\form;
 
+use VGCore\SystemOS;
+
+use VGCore\gui\lib\{
+    LibraryInt,
+    UIDriver,
+    element\Button,
+    element\Dropdown,
+    element\Element,
+    element\Input,
+    element\Label,
+    element\Slider,
+    element\StepSlider,
+    element\Toggle,
+    window\SimpleForm,
+    window\ModalWindow,
+    window\CustomForm
+};
+
 use VGCore\gui\lib\UIBuilder;
 
 class BasicUI extends UIBuilder {
--- src/VGCore/form/StoreUI.php
+++ src/VGCore/form/StoreUI.php
@@ -4,6 +4,24 @@ namespace VGCore\form;
 
 use pocketmine\Player;
 // >>>
+use VGCore\SystemOS;
+
+use VGCore\gui\lib\{
+    LibraryInt,
+    UIDriver,
+    element\Button,
+    element\Dropdown,
+    element\Element,
+    element\Input,
+    element\Label,
+    element\Slider,
+    element\StepSlider,
+    element\Toggle,
+    window\SimpleForm,
+    window\ModalWindow,
+    window\CustomForm
+};
+
 use VGCore\gui\lib\UIBuilder;
 
 use VGCore\store\ItemList as IL;
